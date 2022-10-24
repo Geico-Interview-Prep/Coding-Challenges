6eea330a3e474ede7d93c82805ed9ce104361133
Jean Duong <bao.duong@revature.com>
2022-10-23 21:42:44 -0600

    added comments for warmup

--- Warmup/Anagram/Solution.cs
+++ Warmup/Anagram/Solution.cs
@@ -1,10 +1,16 @@
 namespace Anagram;
 
+/*
+ * TODO:
+ * Implement the method below
+ * 
+ * To get full points on this problem, you must pass all tests uder AnagramTest/SolutionTest.cs
+ * command: dotnet test Warmup/AnagramTest/AnagramTest.csproj
+ */
 public class Solution
 {
     public Boolean IsAnagram(string s1, string s2)
     {
-        /* You can comment this out */
         throw new Exception("Todo! Needs implementation...");
     }
 }
